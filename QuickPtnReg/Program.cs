c2d9ce7a0a316612e404a3e23936c623fbca2651
Sachin Manugade <sachin@sofscript.com>
2025-07-03 17:52:41 +0530

    added serilog log generator. modified error page.

--- QuickPtnReg/Program.cs
+++ QuickPtnReg/Program.cs
@@ -1,7 +1,24 @@
+using Serilog;
+using Serilog.Ui.Web.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Step 1: Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console() // Optional: logs to terminal
+    .WriteTo.File("Logs/app-log-.txt", rollingInterval: RollingInterval.Day) // Logs go into Logs folder
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+// Step 2: Use Serilog as logging provider
+builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+
+
 
 var app = builder.Build();
 
@@ -12,7 +29,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+else
+{
+    app.UseDeveloperExceptionPage(); // For local dev
+}
+ 
 //app.UseHttpsRedirection();
 app.UseStaticFiles();
 
